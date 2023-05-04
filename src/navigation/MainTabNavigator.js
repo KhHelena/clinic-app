edd3b598e32a669d9585e23028ac818257ff9d78
Vladislav Ruban <r.ubanvs20@gmail.com>
2023-05-04 12:29:34 +0300

    Updated Bottom Navigation

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import PatientsScreen from '../screens/PatientsScreen';
 import GroupsScreen from '../screens/GroupsScreen';
+import StatisticsScreen from '../screens/StatisticsScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const Tab = createBottomTabNavigator();
+
+const Tab = createMaterialTopTabNavigator();
 
 const MainTabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Patients" component={PatientsScreen} />
-      <Tab.Screen name="Groups" component={GroupsScreen} />
+    <Tab.Navigator
+      initialRouteName="Patients"
+      
+      tabBarPosition="bottom"
+      screenOptions={({ route }) => ({
+        swipeEnabled: true,
+        tabBarIcon: ({ focused, color }) => {
+          let iconName;
+
+          if (route.name === 'Patients') {
+            iconName = focused ? 'people' : 'people-outline';
+          } else if (route.name === 'Groups') {
+            iconName = focused ? 'albums' : 'albums-outline';
+          } else if (route.name === 'Statistics') {
+            iconName = focused ? 'stats-chart' : 'stats-chart-outline';
+          }
+
+          return <Icon name={iconName} size={24} color={color}/>;
+        },
+      })}
+      tabBarOptions={{
+        showIcon: true,
+        activeTintColor: 'black',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Patients" component={PatientsScreen}/>
+      <Tab.Screen name="Groups" component={GroupsScreen}/>
+      <Tab.Screen name="Statistics" component={StatisticsScreen}/>
     </Tab.Navigator>
   );
 };
--- src/screens/PatientsScreen.js
+++ src/screens/PatientsScreen.js
@@ -1,54 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import { View, TextInput, Modal, Button, FlatList } from 'react-native'
-import PatientListItem from '../components/PatientListItem'
-import PatientForm from '../components/PatientForm'
-import { getPatients, deletePatient, updatePatient, getAllGroups } from '../api'
-import { Picker } from '@react-native-picker/picker'
+import React, { useEffect, useState } from 'react';
+import { View, TextInput, Modal, Button, FlatList, StyleSheet } from 'react-native';
+import PatientListItem from '../components/PatientListItem';
+import PatientForm from '../components/PatientForm';
+import { getPatients, deletePatient, updatePatient, getAllGroups } from '../api';
+import { Picker } from '@react-native-picker/picker';
 
 const PatientsScreen = () => {
-  const [patients, setPatients] = useState([])
-  const [groups, setGroups] = useState([])
-  const [selectedPatient, setSelectedPatient] = useState(null)
-  const [modalVisible, setModalVisible] = useState(false)
-  const [searchNmedcard, setSearchNmedcard] = useState('')
-  const [searchSurname, setSearchSurname] = useState('')
-  const [selectedGroup, setSelectedGroup] = useState('')
-  const [lastLoadedIndex, setLastLoadedIndex] = useState(10)
+  const [patients, setPatients] = useState([]);
+  const [groups, setGroups] = useState([]);
+  const [selectedPatient, setSelectedPatient] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [searchNmedcard, setSearchNmedcard] = useState('');
+  const [searchSurname, setSearchSurname] = useState('');
+  const [selectedGroup, setSelectedGroup] = useState('');
+  const [lastLoadedIndex, setLastLoadedIndex] = useState(10);
 
   useEffect(() => {
-    fetchPatients()
-    fetchGroups()
-  }, [])
+    fetchPatients();
+    fetchGroups();
+  }, []);
 
   const fetchPatients = async () => {
-    const data = await getPatients()
-    setPatients(data)
-  }
+    const data = await getPatients();
+    setPatients(data);
+  };
 
   const fetchGroups = async () => {
-    const data = await getAllGroups()
-    setGroups(data)
-  }
+    const data = await getAllGroups();
+    setGroups(data);
+  };
 
   const handleDelete = async (id) => {
-    await deletePatient(id)
-    fetchPatients()
-  }
+    await deletePatient(id);
+    fetchPatients();
+  };
 
   const handleEdit = (patient) => {
-    setSelectedPatient(patient)
-    setModalVisible(true)
-  }
+    setSelectedPatient(patient);
+    setModalVisible(true);
+  };
 
   const handleUpdate = async (updatedPatient) => {
-    await updatePatient(selectedPatient.Nmedcard, updatedPatient)
-    fetchPatients()
-    setModalVisible(false)
-  }
+    await updatePatient(selectedPatient.Nmedcard, updatedPatient);
+    fetchPatients();
+    setModalVisible(false);
+  };
 
   const closeModal = () => {
-    setModalVisible(false)
-  }
+    setModalVisible(false);
+  };
 
   const filteredPatients = patients
     .filter((patient) => {
@@ -56,12 +56,12 @@ const PatientsScreen = () => {
         patient.Nmedcard.toString().includes(searchNmedcard) &&
         patient.Surname.toLowerCase().includes(searchSurname.toLowerCase()) &&
         (selectedGroup === '' || patient.NGroup === parseInt(selectedGroup))
-      )
+      );
     })
-    .slice(0, lastLoadedIndex)
+    .slice(0, lastLoadedIndex);
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
         placeholder="Search by Nmedcard"
         value={searchNmedcard}
@@ -74,14 +74,11 @@ const PatientsScreen = () => {
       />
       <Picker
         selectedValue={selectedGroup}
-        onValueChange={(itemValue) => setSelectedGroup(itemValue)}>
+        onValueChange={(itemValue) => setSelectedGroup(itemValue)}
+      >
         <Picker.Item label="All groups" value="" />
         {groups.map((group) => (
-          <Picker.Item
-            key={group.NGroup}
-            label={group.Name}
-            value={group.NGroup.toString()}
-          />
+          <Picker.Item key={group.NGroup} label={group.Name} value={group.NGroup.toString()} />
         ))}
       </Picker>
       <FlatList
@@ -96,13 +93,18 @@ const PatientsScreen = () => {
         )}
         onEndReached={() => setLastLoadedIndex(lastLoadedIndex + 10)}
         onEndReachedThreshold={0.5}
-      />
-      <Modal visible={modalVisible} onRequestClose={closeModal}>
-        <PatientForm patient={selectedPatient} onSubmit={handleUpdate} />
-        <Button title="Cancel" onPress={closeModal} />
-      </Modal>
-    </View>
-  )
-}
+        />
+        <Modal visible={modalVisible} onRequestClose={closeModal}>
+          <PatientForm patient={selectedPatient} onSubmit={handleUpdate} />
+          <Button title="Cancel" onPress={closeModal} />
+        </Modal>
+      </View>
+);
+};
 
-export default PatientsScreen
+const styles = StyleSheet.create({
+  container: {
+    // backgroundColor: '#ebfaff'
+  }
+})
+export default PatientsScreen;      

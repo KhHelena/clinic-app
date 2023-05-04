4abad67eb2399a92d3f70fed6998fc0119bec4ab
Vladislav Ruban <r.ubanvs20@gmail.com>
2023-05-04 12:02:50 +0300

    Updated patients design

--- src/components/PatientListItem.js
+++ src/components/PatientListItem.js
@@ -1,32 +1,99 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString)
+  return `${('0' + date.getDate()).slice(-2)}.${(
+    '0' +
+    (date.getMonth() + 1)
+  ).slice(-2)}.${date.getFullYear()}`
+}
 
 const PatientListItem = ({ patient, onDelete, onEdit }) => {
   return (
-    <View>
-      <Text>
-        {patient.Surname} {patient.FirstName} {patient.Patronymic}
-      </Text>
-      <Text>Address: {patient.Adress}</Text>
-      <Text>Date of Birth: {patient.DataOfBirth}</Text>
-      <Text>Sex: {patient.Sex}</Text>
-      <Text>Height: {patient.Height}</Text>
-      <Text>Weight: {patient.Weight}</Text>
-      <Text>Phone: {patient.PhoneN}</Text>
-      <Text>Email: {patient.Email}</Text>
-      <Text>Diagnosis: {patient.Diagnosis}</Text>
-      <Text>Group: {patient.NGroup}</Text>
-      <Text>NSessionDiagn: {patient.NSessionDiagn}</Text>
-      <Text>NSessionTreat: {patient.NSessionTreat}</Text>
-      <Text>Nmedcard: {patient.Nmedcard}</Text>
-      <TouchableOpacity onPress={() => onEdit(patient)}>
-        <Text>Edit</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => onDelete(patient.Nmedcard)}>
-        <Text>Delete</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      {patient.Sex === 'м.' ? (
+        <Icon name="man" size={40} color="#6bdaff" />
+      ) : (
+        <Icon name="woman" size={40} color="#ff94fd" />
+      )}
+      <View style={styles.info}>
+        <Text>
+          {patient.Surname} {patient.FirstName} {patient.Patronymic}
+        </Text>
+        <View style={styles.columns}>
+          <View style={styles.column}>
+            <Text>{formatDate(patient.DataOfBirth)}</Text>
+            <Text>📞 {patient.PhoneN}</Text>
+            <Text>Email: {patient.Email}</Text>
+            <Text>Адреса: {patient.Adress}</Text>
+          </View>
+          <View style={styles.column}>
+            <Text>
+              {patient.Height} см / {patient.Weight} кг
+            </Text>
+            <Text>Діагноз: {patient.Diagnosis}</Text>
+            <Text>NSessionDiagn: {patient.NSessionDiagn}</Text>
+            <Text>NSessionTreat: {patient.NSessionTreat}</Text>
+          </View>
+        </View>
+        <View style={styles.footer}>
+          <View style={styles.buttons}>
+            <TouchableOpacity
+              onPress={() => onEdit(patient)}
+              style={styles.button}>
+              <Icon name="create-outline" size={24} color="#333" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => onDelete(patient.Nmedcard)}
+              style={styles.button}>
+              <Icon name="trash-outline" size={24} color="#333" />
+            </TouchableOpacity>
+          </View>
+          <Text>
+            Мед. Карта: {patient.Nmedcard} Група: {patient.NGroup}
+          </Text>
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    marginVertical: 5,
+    marginHorizontal: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 8
+  },
+  info: {
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+  columns: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+  column: {
+    flex: 1,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    marginLeft: 8,
+  },
+})
 
 export default PatientListItem

637a93eef82f7dce23912cc01e0ec1f4d1f73e79
Vladislav Ruban <r.ubanvs20@gmail.com>
2023-05-06 09:07:20 +0300

    detailed sessions screen + created table and chart

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainTabNavigator from './src/navigation/MainTabNavigator';
+import 'react-native-svg';
 
 export default function App() {
   return (
